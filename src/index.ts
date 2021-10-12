da20fe9488aebf0bcd0d690ea0078c315f98d7f2
euri10 <benoit.barthelet@gmail.com>
2021-10-12 20:27:19 +0200

    feat: created barcode TS types and Java param reading
    Trying something
    
    Passing formats to integers
    
    Misplaced brackets ?
    
    Better with a real IDE
    
    Instanciation mystery for a java noob
    
    Ok error: illegal start of type , but why ?
    
    Not sure
    
    Not sure 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import type { Frame } from 'react-native-vision-camera';
 
 /**
+  * @see https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/Barcode.BarcodeFormat
+  */
+export enum CodeFormat {
+  FORMAT_UNKNOWN = -1,
+  FORMAT_ALL_FORMATS = 0,
+  FORMAT_CODE_128 = 1,
+  FORMAT_CODE_39 = 2,
+  FORMAT_CODE_93 = 4,
+  FORMAT_CODABAR = 8,
+  FORMAT_DATA_MATRIX = 16,
+  FORMAT_EAN_13 = 32,
+  FORMAT_EAN_8 = 64,
+  FORMAT_ITF = 128,
+  FORMAT_QR_CODE = 256,
+  FORMAT_UPC_A = 512,
+  FORMAT_UPC_E = 1024,
+  FORMAT_PDF417 = 2048,
+  FORMAT_AZTEC = 4096,
+}
+/**
  * @see https://developers.google.com/android/reference/com/google/mlkit/vision/barcode/Barcode.BarcodeValueType
  */
 export enum QrCodeType {
