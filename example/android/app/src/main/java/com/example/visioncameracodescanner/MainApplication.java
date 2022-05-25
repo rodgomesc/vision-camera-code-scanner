ac2f072700d128b4f91cf9af308ed415d5132183
Daniel Chong <danielgq.chong@gmail.com>
2022-05-25 17:24:21 +1200

    trying to get it to work

--- example/android/app/src/main/java/com/example/visioncameracodescanner/MainApplication.java
+++ example/android/app/src/main/java/com/example/visioncameracodescanner/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.visioncameracodescanner.VisionCameraCodeScannerPluginPackage;
+import com.visioncameraocr.VisionCameraOcrPackage;
 
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for VisionCameraCodeScannerExample:
           // packages.add(new MyReactNativePackage());
           packages.add(new VisionCameraCodeScannerPluginPackage());
+          packages.add(new VisionCameraOcrPackage());
           return packages;
         }
 
