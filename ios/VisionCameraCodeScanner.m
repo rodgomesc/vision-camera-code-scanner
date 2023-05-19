bc6452486e7cec0f9740c20841313e1cfeb61507
Preston Davis <admin@anomaly.app>
2023-05-18 18:02:54 -0700

    Conditional import of Swift interface header

--- ios/VisionCameraCodeScanner.m
+++ ios/VisionCameraCodeScanner.m
@@ -1,7 +1,11 @@
 #import <Foundation/Foundation.h>
 
 #import "VisionCameraCodeScanner.h"
+#if defined __has_include && __has_include("VisionCameraCodeScanner-Swift.h")
 #import "VisionCameraCodeScanner-Swift.h"
+#else
+#import <VisionCameraCodeScanner/VisionCameraCodeScanner-Swift.h>
+#endif
 
 @implementation RegisterPlugins
 
