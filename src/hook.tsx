dedefa68850228de41e8978123c1a4ec6e338134
rkmackinnon <129107344+rkmackinnon@users.noreply.github.com>
2023-05-30 11:39:26 -0700

    Add comment to hook.tsx

--- src/hook.tsx
+++ src/hook.tsx
@@ -17,6 +17,11 @@ export function useScanBarcodes(
 
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet';
+    // For some reason, arguments to hooks accessed from worklets don't have the same runtime type they
+    // are declared as. This leads to tricky errors, hence the Array.from and reduce on the options' keys.
+    //
+    // Example error:
+    // Frame Processor threw an error: Exception in HostFunction: Received an unknown HostObject! Cannot convert to a JNI value.
     const opts = Object.keys(options || {}).reduce((acc, key) => {
       acc[key as keyof CodeScannerOptions] = (options || {})[
         key as keyof CodeScannerOptions
